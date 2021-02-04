b48200c22ef62f8695bcf826d0c58c9b6d0165de
TheCoolestPaul <pack299cubstars@gmail.com>
2021-02-03 20:03:24 -0600

    Fixed bug with mending

--- src/main/java/net/thirdshift/paulstweaks/event/BlockBreakEvent.java
+++ src/main/java/net/thirdshift/paulstweaks/event/BlockBreakEvent.java
@@ -17,15 +17,12 @@ public class BlockBreakEvent {
 	@SubscribeEvent
 	public static void onBlockBreak(BlockEvent.BreakEvent event){
 		if (event.getPlayer()!=null && event.getPlayer().getHeldItemMainhand().isEnchanted()){
-			int level = EnchantmentHelper.getEnchantmentLevel(STONE_MENDING.get(), event.getPlayer().getHeldItemMainhand());
-			if (level==0)
-				level = EnchantmentHelper.getEnchantmentLevel(NETHER_MENDING.get(), event.getPlayer().getHeldItemMainhand());
-			if (level>0) {
-				if (event.getState().isIn(BlockTags.BASE_STONE_OVERWORLD)) {
-					StoneMendingEnchantment.stoneMend(event.getPlayer().getHeldItemMainhand(), level);
-				} else if (event.getState().isIn(BlockTags.BASE_STONE_NETHER))
-					NetherMendingEnchant.netherMend(event.getPlayer().getHeldItemMainhand(), level);
-			}
+			int stoneLevel = EnchantmentHelper.getEnchantmentLevel(STONE_MENDING.get(), event.getPlayer().getHeldItemMainhand());
+			int netherLevel = EnchantmentHelper.getEnchantmentLevel(NETHER_MENDING.get(), event.getPlayer().getHeldItemMainhand());
+			if (event.getState().isIn(BlockTags.BASE_STONE_OVERWORLD) && stoneLevel > 0) {
+				StoneMendingEnchantment.stoneMend(event.getPlayer().getHeldItemMainhand(), stoneLevel);
+			} else if (event.getState().isIn(BlockTags.BASE_STONE_NETHER) && netherLevel > 0)
+				NetherMendingEnchant.netherMend(event.getPlayer().getHeldItemMainhand(), netherLevel);
 		}
 	}
 }
